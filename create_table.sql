d8048ad236353ce2e2c6a470388c44718838241b
putragabrielll <putragmahk@gmail.com>
2023-11-07 16:30:14 +0700

    update query database

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,4 @@
+-- Create table users
 create table if not exists "users"(
 "id" serial primary key,
 "name" varchar (255) unique not null,
@@ -10,7 +11,7 @@ create table if not exists "users"(
 "createdAt" timestamp default now(),
 "updatedAt" timestamp 
 );
-
+-- Create table product
 create table if not exists "product"(
 "id" serial primary key,
 "name" varchar (255) not null,
@@ -22,7 +23,7 @@ create table if not exists "product"(
 "createdAt" timestamp default now(),
 "updatedAt" timestamp 
 );
-
+-- Create table promo
 create table if not exists "promo"(
 "id" serial primary key,
 "code" varchar(20) not null,
@@ -36,7 +37,7 @@ create table if not exists "promo"(
 "createdAt" timestamp default now(),
 "updatedAt" timestamp 
 );
-
+-- Create table order
 create table if not exists "order"(
 "id" serial primary key,
 "user_id" int not null,
@@ -52,3 +53,61 @@ foreign key (promo_id) references promo(id),
 "createdAt" timestamp default now(),
 "updatedAt" timestamp 
 );
+
+
+-- CRUD Table Users
+INSERT INTO users
+(id, "name", "level", images, phone, alamat, email, "password", "createdAt", "updatedAt")
+VALUES(1, 'gabriel', 'user', '1.jpg', '085155556666', 'Bekasi', 'puragmahk@gmail.com', 'putra123', '2023-11-06 21:18:37.109', NULL);
+
+UPDATE users
+SET "name"='gabriel', "level"='user', images='', phone='0808080089832', alamat='Bekasi', email='puragmahk@gmail.com', "password"='putra123', "createdAt"='2023-11-06 21:18:37.109', "updatedAt"=NULL
+WHERE id=1;
+
+DELETE FROM users WHERE id=1;
+
+SELECT * FROM users
+
+
+
+-- CRUD Table Product
+INSERT INTO product (id, name_product, images, desk_product, qty, status, "createdAt", "updatedAt")
+VALUES(1, 'kopi susu', 'kopi-susu.png', NULL, 5, true, '2023-11-06 21:31:19.938', NULL);
+
+UPDATE product SET name_product='kopi susu', images='kopi-susu.png', desk_product=NULL, qty=5, status=true, "createdAt"='2023-11-06 21:31:19.938', "updatedAt"=NULL
+WHERE id=1;
+
+DELETE FROM product WHERE id=1;
+
+SELECT * FROM product
+
+
+-- CRUD Table Promo
+INSERT INTO promo (id, code, images, min_order, max_discount, "percent", description, qty, status, "createdAt", "updatedAt")
+VALUES(1, 'FAZZFOOD50', 'promo.png', 50000, 50000, 0.5, NULL, 100, true, '2023-11-06 22:13:56.332', NULL);
+
+UPDATE promo SET code='FAZZFOOD50', images='promo.png', min_order=50000, max_discount=50000, "percent"=0.5, description=NULL, qty=100, status=true, "createdAt"='2023-11-06 22:13:56.332', "updatedAt"=NULL
+WHERE id=1;
+
+DELETE FROM promo WHERE id=1;
+
+SELECT * FROM promo
+
+
+-- CRUD Table Order
+INSERT INTO public."order" (id, user_id, product_id, promo_id, date_order, qty, status_order, noted, "createdAt", "updatedAt")
+VALUES(2, 1, 1, 1, '2023-11-06', 2, 1, 'siap bos kuu.', '2023-11-06 22:17:47.144', NULL);
+
+UPDATE public."order" SET user_id=1, product_id=1, promo_id=1, date_order='2023-11-06', qty=2, status_order=1, noted='siap bos kuu.', "createdAt"='2023-11-06 22:17:47.144', "updatedAt"=NULL
+WHERE id=2;
+
+DELETE FROM order WHERE id=2;
+
+SELECT * FROM order
+
+
+-- penggunaan ALTER untuk menambahkan column baru lagi
+alter table "product" add column "price" decimal not null
+
+-- penggunaan ALTER untuk mengganti nama column
+alter table "product" rename column "desc_product" to "description"
